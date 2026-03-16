fcde4b6f1630b835e05d67400ad256217c7af31e
yankey <nwolle14@gmail.com>
2026-03-16 03:10:07 +0000

    Ajout du server.js

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+
+// charger les variables d'environnement
+dotenv.config();
+
+const app = express()
+const port = 4000
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`l application sera lancer sur le port : ${port}`)
+})
