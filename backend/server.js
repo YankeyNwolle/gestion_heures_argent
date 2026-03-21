576ad25f12f76bc09e3a99d4a33d922a02a581f9
yankey <nwolle14@gmail.com>
2026-03-21 22:56:32 +0000

    Ajout d'enpoints d'API

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const authRoutes = require("./routes/authRoutes");
 
 
 // charger les variables d'environnement
@@ -9,14 +10,12 @@ dotenv.config();
 const app = express()
 const port = 4000
 
-app.use(cors());
+app.use(cors());  // autorise toutes les requêtes
 app.use(express.static('public'))
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
-  console.log(`l application sera lancer sur le port : ${port}`)
+  console.log(`serveur lancé sur le port  : ${port}`)
 })
